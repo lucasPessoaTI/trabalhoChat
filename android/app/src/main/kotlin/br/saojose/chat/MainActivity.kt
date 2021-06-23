846aa5232dab479ceecc8ee502db620970c5f1b6
lucas pessoa <lucaspessoa.gomes@gmail.com>
2021-06-22 22:41:02 -0300

    first commit

---
+++ android/app/src/main/kotlin/br/saojose/chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.saojose.chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/const.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+final themeColor = Color(0xffB22222);
+final primaryColor = Color(0xffFFA07A);
+final greyColor = Color(0xffaeaeae);
+final greyColor2 = Color(0xffE8E8E8);
